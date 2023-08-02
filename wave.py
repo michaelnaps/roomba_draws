70e862021e20f8440ed8e9a2d450ccaba02dfac4
Michael Napoli <mnapoli@irobot.com>
2023-08-02 10:33:57 -0400

    new plan

--- wave.py
+++ wave.py
@@ -14,11 +14,12 @@ import MPC.Plant as plant
 import MPC.Optimizer as opt
 
 # Hyper parameter(s)
-dt = 0.02
+A = 1
+dt = A*0.02
 P = 12
 k = 2
-R = 0.25
-L = R + 0.10  # marker offset
+R = A*0.25
+L = R + A*0.10  # marker offset
 Nx = 3
 Nu = 2
 
@@ -53,13 +54,12 @@ def cost(xList, uList):
 
 # Generate path list.
 def path(xcoord):
-    # ycoord = np.cos( xcoord )
+    xcoord = np.array( xcoord )/A
     ycoord = 0.25*np.cos( xcoord ) \
         + 0.4*np.cos( xcoord )**2 \
         + np.cos( xcoord )**3 \
         + 0.1*np.cos( xcoord )**4
-    # ycoord = np.sign( xcoord )
-    return ycoord
+    return A*ycoord
 
 def pathSteps(xstate, N=P+1, beta=dt):
     xcoord = xstate[0]
@@ -81,7 +81,7 @@ if __name__ == "__main__":
     mpc_var = opt.ModelPredictiveControl( model, cost,
         P=P, k=k, Nx=Nx, Nu=Nu, dt=dt,
         cost_type='horizon' )
-    mpc_var.setStepSize( 1.00 )
+    mpc_var.setStepSize( 1.00/A )
 
     uinit = np.zeros( (Nu,P) )
     mpc_var.setMaxIter( 1000 )
@@ -89,7 +89,7 @@ if __name__ == "__main__":
     mpc_var.setMaxIter( 10 )
 
     # Simulation series.
-    T = 10;  Nt = round( T/dt ) + 1
+    T = A*10;  Nt = round( T/dt ) + 1
     tList = np.array( [[i for i in range( Nt )]] )
     pList = pathSteps( x0, N=Nt )
     # xpred = mpc_var.statePrediction( x0, uinit )
@@ -107,10 +107,14 @@ if __name__ == "__main__":
 
     # Initialize forward tail and plot.
     xpred = mpc_var.statePrediction( x0, uList )[:2,:]
-    v_var.initForwardTail( xpred )
+    v_var.initForwardTail( xpred, zorder=20 )
     marker.linestyle = ':'  # manually set linestyle
     v_var.draw()
     marker.draw()
+
+    # plt.axis( [-6, 6, -6, 6] )
+    plt.gca().set_aspect( 'equal', adjustable='box' )
+    plt.show( block=0 )
 
     # Simulation loop.
     x = x0
@@ -126,8 +130,9 @@ if __name__ == "__main__":
 
         # Update state and animation.
         x = m_var.prop( x, u[:,0,None] )
-        v_var.update( x[:2], pause=0 )
+        v_var.update( x[:2] )
         marker.update( markerPosition( x ) )
+        plt.pause( 1e-3 )
 
         # Break if sim exceeds boundaries of T.
         if x[0] > T:
