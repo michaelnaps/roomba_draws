da5d8e768be2d5c387cdfb21b907a4c6a1456041
Michael Napoli <mikednapoli@gmail.com>
2023-08-09 19:04:03 -0400

    recorded gif with vec for pres

--- sketch.py
+++ sketch.py
@@ -14,18 +14,19 @@ import GEOM.Vehicle2D as vhc
 import GEOM.Vectors as vect
 import FOUR.Transforms as four
 
-dt = 0.1
+dt = 0.25
+W = 1
 
 # Main execution loop.
 if __name__ == "__main__":
     # Import hat data and fit to necessary size.
     file = expanduser('~')+'/prog/four/abby_pkg/sketchdata.csv'
     data = pd.read_csv( file )
-    xTrain = (1/20)*data[ data['z']=='d' ].to_numpy()[::15,:2].T
+    xTrain = data[ data['z']=='d' ].to_numpy()[::15,:2].T
 
     # Calculate Fourier transform over x-y sets.
     nTrain = xTrain.shape[1]
-    tTrain = np.array( [[i for i in range( nTrain )]] )
+    tTrain = W*np.array( [[i for i in range( nTrain )]] )
     fvar = four.RealFourier( tTrain, xTrain )
     fvar.dmd( N=25 )
 
@@ -35,6 +36,12 @@ if __name__ == "__main__":
     Nt = round( nTrain/dt )
     tList = np.array( [[i*dt for i in range( Nt )]] )
     pList = fvar.solve( tTrain )
+
+    print( 'x:', min( xTrain[0] ), max( xTrain[0] ) )
+    print( 'y:', min( xTrain[1] ), max( xTrain[1] ) )
+
+    ox = np.array( [[0],[-25]] )
+    oy = np.array( [[-125],[0]] )
 
     # Vehicle variable and static initializations.
     fig, axs = plt.subplots()
@@ -47,11 +54,11 @@ if __name__ == "__main__":
 
     # Draw Fourier vectors.
     fvect = fvar.vectors( t0 )
-    xvect = vect.Vectors( fvect[0], fig=fig, axs=axs, color='k' )
-    dxvect = vect.Vectors( np.hstack( (fvect[0][:,-1,None], x0) ),
+    xvect = vect.Vectors( fvect[0]+ox, fig=fig, axs=axs, color='k' )
+    dxvect = vect.Vectors( np.hstack( (fvect[0][:,-1,None]+ox, x0) ),
         fig=fig, axs=axs, color='grey' )
-    yvect = vect.Vectors( np.flipud( fvect[1] ) + [[-5],[0]], fig=fig, axs=axs, color='k' )
-    dyvect = vect.Vectors( np.hstack( (np.flipud( fvect[1][:,-1,None] )+[[-5],[0]], x0) ),
+    yvect = vect.Vectors( np.flipud( fvect[1] )+oy, fig=fig, axs=axs, color='k' )
+    dyvect = vect.Vectors( np.hstack( (np.flipud( fvect[1][:,-1,None] )+oy, x0) ),
         fig=fig, axs=axs, color='grey' )
 
     # Initialize forward tail and plot.
@@ -63,7 +70,7 @@ if __name__ == "__main__":
     yvect.draw()
     dyvect.setLineStyle( ':' );  dyvect.draw()
 
-    plt.axis( [-10, 20, -10, 20] )
+    plt.axis( [-250, 400, -150, 400] )
     plt.gca().set_aspect( 'equal', adjustable='box' )
     plt.show( block=0 )
 
@@ -74,10 +81,10 @@ if __name__ == "__main__":
         x = fvar.solve( t[:,None] )
         fvect = fvar.vectors( t[:,None] )
 
-        xvect.setVertices( fvect[0] )
-        dxvect.setVertices( np.hstack( (fvect[0][:,-1,None], x) ) )
-        yvect.setVertices( np.flipud( fvect[1] ) + [[-5],[0]] )
-        dyvect.setVertices( np.hstack( ( np.flipud( fvect[1][:,-1,None] )+[[-5],[0]], x) ) )
+        xvect.setVertices( fvect[0]+ox )
+        dxvect.setVertices( np.hstack( (fvect[0][:,-1,None]+ox, x) ) )
+        yvect.setVertices( np.flipud( fvect[1] )+oy )
+        dyvect.setVertices( np.hstack( ( np.flipud( fvect[1][:,-1,None] )+oy, x) ) )
 
         v_var.update( x )
         xvect.update()
